6e5ea4cdc75c1680d4d0f4d7e646072b29da4bce
Ziliang Guo <ziliang@cs.wisc.edu>
2012-02-24 21:12:13 -0600

    Add project solution and files.

---
+++ JsonPrototype/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace JsonPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding encoding = System.Text.Encoding.GetEncoding("utf-8");
+            User userData = new User();
+            userData.Name = "u1-0@example.com";
+            userData.Password = "letmein";
+            HttpWebRequest request = WebRequest.Create("http://trails_forward.dev.mirerca.com/users/authenticate_for_token") as HttpWebRequest;
+            request.ContentType = "application/json; charset=utf-8";
+            request.Method = "post";
+            Stream jsonStream = request.GetRequestStream();
+            Console.WriteLine(userData.ToString());
+            DataContractJsonSerializer jsonSerial = new DataContractJsonSerializer(typeof(User));
+            jsonSerial.WriteObject(jsonStream, userData);
+            jsonStream.Flush();
+            //jsonStream.Close();
+            
+            HttpWebResponse response = null;
+            StreamReader readStream = null;
+            
+            try
+            {
+                response = request.GetResponse() as HttpWebResponse;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            Stream recStream = response.GetResponseStream();
+            
+            Char[] read = new Char[256];
+            int count = readStream.Read(read, 0, 256);
+            while (count > 0)
+            {
+                string str = new string(read, 0, count);
+                Console.Write(str);
+                count = readStream.Read(read, 0, 256);
+            }
+            response.Close();
+        }
+    }
+}
---
+++ JsonPrototype/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace JsonPrototype
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        internal string fullName = "user";
+        [DataMember]
+        private string name;
+        [DataMember]
+        private string password;
+
+        public string Name
+        {
+            get { return Name; }
+            set { name = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
